4af71ea9e9295abe53dfc2124579c635b951dab8
dionisis-deligiannis <delisdionysios@gmail.com>
2024-03-11 10:50:27 +0200

    first commit and basic setup

---
+++ index.js
@@ -0,0 +1,7 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const upperCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-="
+
