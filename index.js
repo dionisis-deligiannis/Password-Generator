87e68414ea53a4e581781861a7ca55b8e0e44244
dionisis-deligiannis <delisdionysios@gmail.com>
2024-03-11 11:42:33 +0200

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const passwordBox = document.getElementById("password");
+const generateButton = document.getElementById("btn");
+const clipboard = document.getElementById("clipboard")
 const length = 12;
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
-const symbol = "@#$%^&*()_+~|}{[]></-="
+const symbol = "@#$%^&*()_+~|}{[]></-=";
 const allChars = upperCase + lowerCase + number + symbol;
 
 function createPassword(){
@@ -11,8 +13,15 @@ function createPassword(){
     password += upperCase[ Math.floor(Math.random()*lowerCase.length)];
     password += lowerCase[ Math.floor(Math.random()*lowerCase.length)];
     password += number[ Math.floor(Math.random()*number.length)];
-    password += number[ Math.floor(Math.random()*symbol.length)];
+    password += symbol[ Math.floor(Math.random()*symbol.length)];
     while(length > password.length){
         password += allChars[ Math.floor(Math.random()* allChars.length)]
     }
+    passwordBox.value = password;
 }
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
+generateButton.addEventListener("click",createPassword)
+clipboard.addEventListener("click", copyPassword)
